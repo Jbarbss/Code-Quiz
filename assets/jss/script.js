87ea364dbd40b456f6d4c081c0418c871b895f93
Jbarbss <jbarbanel@gmail.com>
2021-03-03 16:49:20 -0500

    added function for start button onclick for title screen to disapear and question container to show up and timer to start

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,3 +1,34 @@
-var startButton = document.getElementById("startBtn");
-var timerElement = document.getElementById("timer"); 
+var startBtn = document.getElementById("startBtn");
+var timerEl = document.getElementById("time"); 
+var submitBtn = document.getElementById("submitBtn");
+var titleScreen = document.getElementById("intro");
+var quizScreen = document.getElementById("quizBox");
+var resetBtn = document.getElementById("reset");
+var clearBtn = document.getElementById("clearScore")
+var scoreDis = document.getElementById("scoreDisplay")
+var questionEl = document.getElementById("questions")
+var answerBtn = document.getElementById("answerBtn")
+
+var quizTimer;
 var timerStart = 75;
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    console.log("Started");
+    titleScreen.classList.add("hide")
+    quizScreen.classList.remove("hide")
+    scoreDis.classList.remove("hide")
+    timerStart = 75;
+    quizTimer = setInterval(timeAlert, 1000);
+}
+
+function timeAlert() {
+    if (timerStart > 0) {
+        
+        timerStart--;
+       return timerEl.textContent = timerStart;
+    }
+    clearInterval(quizTimer);
+}
+
