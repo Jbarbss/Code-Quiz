7c08fca514ff785f62f6d256fffc761a2c145b34
Jbarbss <jbarbanel@gmail.com>
2021-03-04 16:30:24 -0500

    redesigned css added (quiz finished with your score and to enter your name) updated jscript

--- assets/jss/questions.js
+++ assets/jss/questions.js
@@ -1,28 +1,55 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      question: "Commonly used data types DO NOT include:",
+      answers :  [
+          { text: "alerts", correct: true },
+          { text: "booleans", correct: false },
+          { text: "strings", correct: false },
+          { text: "numbers", correct: false },
+      ]    
     },
+
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      title: "Which of these is not used to loop?",
-      choices: ["for", "while", "foreach", "sequence"],
-      answer: "sequence"
-    },
-    {
-      title: "Which of these is not a way to save a variable",
-      choices: ["vet", "var", "let", "const"],
-      answer: "vet"
-    },
-    {
-      title: "JS date function starts in seconds to current day from what day in 1970",
-      choices: ["January 1", "December 31", "June 1", "April 23"],
-      answer: "January 1"
-    },
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answers :  [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parentheses", correct: true },
+            { text: "square brackets", correct: false },
+        ]    
+      },
+
+      {
+        question: "Which of these is not used to loop?",
+        answers :  [
+            { text: "for", correct: false },
+            { text: "while", correct: false },
+            { text: "foreach", correct: false },
+            { text: "sequence", correct: true },           
+        ]    
+      },
+
+      {
+        question: "Which of these is not a way to save a variable",
+        answers :  [
+            { text: "vet", correct: false },
+            { text: "var", correct: true },
+            { text: "let", correct: false },
+            { text: "const", correct: false },
+        ]    
+      },
+
+      {
+        question: "JS date function starts in seconds to current day from what day in 1970",
+        answers :  [
+            { text: "January 1", correct: true },
+            { text: "December 31", correct: false },
+            { text: "May 29", correct: false },
+            { text: "April 3", correct: false },
+        ]    
+      }
+];
    
-  ];
+    
+
+ 
--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,34 +1,184 @@
-var startBtn = document.getElementById("startBtn");
-var timerEl = document.getElementById("time"); 
-var submitBtn = document.getElementById("submitBtn");
+var startButton = document.getElementById("start-btn");
+var questionContainerEl = document.getElementById("question-container");
+var timerEl = document.getElementById("time");
 var titleScreen = document.getElementById("intro");
-var quizScreen = document.getElementById("quizBox");
-var resetBtn = document.getElementById("reset");
-var clearBtn = document.getElementById("clearScore")
-var scoreDis = document.getElementById("scoreDisplay")
-var questionEl = document.getElementById("questions")
-var answerBtn = document.getElementById("answerBtn")
-
+var questionElement = document.getElementById("question");
+var answerButtonElement = document.getElementById("answer-buttons");
+var nextButton = document.getElementById("next-btn");
+var submitScore = document.getElementById('submit-score');
+var startScreen = document.getElementById("startIntro");
 var quizTimer;
-var timerStart = 75;
 
-startBtn.addEventListener('click', startQuiz);
+let timerTotal = 75;
+let shuffledQuestions, currentQuestionIndex;
+let correctAnswers = 0;
 
-function startQuiz() {
-    console.log("Started");
-    titleScreen.classList.add("hide")
-    quizScreen.classList.remove("hide")
-    scoreDis.classList.remove("hide")
-    timerStart = 75;
-    quizTimer = setInterval(timeAlert, 1000);
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  startButton.classList.add("hide");
+  titleScreen.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerEl.classList.remove("hide");
+
+  timeAlert();
+
+  setNextQuestion();
 }
 
 function timeAlert() {
-    if (timerStart > 0) {
-        
-        timerStart--;
-       return timerEl.textContent = timerStart;
+  quizTimer = setInterval(() => {
+    if (timerTotal > 0) {
+      timerEl.textContent = timerTotal;
+      timerTotal--;
+    } else if (timerTotal === 0) {
+      timerEl.textContent = timerTotal;
+      timerTotal--;
+    } else {
+      gameOver();
     }
-    clearInterval(quizTimer);
+  }, 1000);
 }
 
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonElement.firstChild) {
+    answerButtonElement.removeChild(answerButtonElement.firstChild);
+  }
+}
+function selectAnswer(e) {
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  if (!correct) {
+    timerTotal -= 10;
+  }
+  Array.from(answerButtonElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    gameOver()
+  }
+  if (selectedButton.dataset = correct) {
+      correctAnswers +=10;
+  }
+  console.log(correctAnswers);
+document.getElementById("correctAnswers").innerHTML = correctAnswers
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+   element.classList.remove("correct");
+   element.classList.remove("wrong");
+}
+
+function gameOver(quizTimer) {
+    questionContainerEl.classList.add("hide")
+    startScreen.classList.add("hide")
+    submitScore.classList.remove("hide")
+clearInterval(quizTimer)
+    
+}
+
+function addScore () {
+    userNameInput = document.getElementById('userName').value.trim()
+
+//create a object with name and score
+    var newScore = {
+        name: userNameInput,
+        score: correctAnswers
+    };
+    console.log(newScore);
+    // check if there are any scores in local storage first and take value
+    //if not, make a blank array
+    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+    // push object into score array
+    highScores.push(newScore)
+    // turn objects into an array of strings + put it into local storage
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+
+// var startBtn = document.getElementById("startBtn");
+// var timerEl = document.getElementById("time");
+// var submitBtn = document.getElementById("submitBtn");
+// var titleScreen = document.getElementById("intro");
+// var quizScreen = document.getElementById("quizBox");
+// var resetBtn = document.getElementById("reset");
+// var clearBtn = document.getElementById("clearScore")
+// var scoreDis = document.getElementById("scoreDisplay")
+// var questionEl = document.getElementById("questions")
+// var answerBtn = document.getElementById("answerBtn")
+// var choices = document.getElementById("choice")
+
+// var quizTimer;
+// var timerStart = 75;
+// var finalQuestionIndex = quizQuestions.length;
+// var currentQuestionIndex = 0;
+// var score = 0;
+
+// startBtn.addEventListener('click', startQuiz);
+
+// function startQuiz() {
+//     console.log("Started");
+//     titleScreen.classList.add("hide")
+//     quizScreen.classList.remove("hide")
+//     // scoreDis.classList.remove("hide")
+//     timerStart = 75;
+//     quizTimer = setInterval(timeAlert, 1000);
+//    showQuestion();
+// }
+
+// function timeAlert() {
+//     if (timerStart > 0) {
+
+//         timerStart--;
+//        return timerEl.textContent = timerStart;
+//     }
+
+//     clearInterval(quizTimer);
+// }
+
+// function showQuestion() {
+//     for (var i = 0; i < quizQuestions.length; i++) {
+//     var currentQuestion = quizQuestions[currentQuestionIndex];
+//     questionEl.innerHTML = currentQuestion.title;
+//     choices.textContent = currentQuestion.choices[i];
+
+//     }
+// }
